dd27b527eca32d7aae15180e84ccb22066a07c4e
raychong <raychongtk@gmail.com>
2021-08-01 22:08:10 +0800

    init http client

---
+++ redis_client/lib/http/http_client.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class HttpClient {
+  Future<http.Response> get(String url, {Map<String, String>? headers}) async {
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
+    return response;
+  }
+
+  Future<http.Response> post(String url,
+      {Map<String, String>? headers, Map<String, String>? body}) async {
+    http.Response response = await http.post(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
+    return response;
+  }
+
+  Future<http.Response> put(String url,
+      {Map<String, String>? headers, Map<String, String>? body}) async {
+    http.Response response = await http.put(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
+    return response;
+  }
+
+  Future<http.Response> delete(String url,
+      {Map<String, String>? headers, Map<String, String>? body}) async {
+    http.Response response = await http.delete(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
+    return response;
+  }
+}
